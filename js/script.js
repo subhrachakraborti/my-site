9fd304e9c51d1f3712c5d618425dbda69fc714d2
Subhra Chakraborti <cers.subhra@gmail.com>
2024-09-13 22:15:01 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//Clarity tracking code for https://subhrachakraborti.com/
+(function(c, l, a, r, i, t, y) {
+    c[a] = c[a] || function() {
+        (c[a].q = c[a].q || []).push(arguments)
+    };
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "l2mqlz12d7");
+
+(function(c, l, a, r, i, t, y) {
+    c[a] = c[a] || function() {
+        (c[a].q = c[a].q || []).push(arguments)
+    };
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "l2mqlz12d7");
+
+//<!-- Google Tag Manager -->
+(function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-5M7ZMQNV');
+//<!-- End Google Tag Manager -->
+//<!-- Google tag (gtag.js) -->
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-QK8MWKMS3M');
+
+//end code
+
+
+
+function goBack() {
+    window.history.back();
+}
+
+function goHome() {
+    window.open("https://subhrachakraborti.com/", "_self");
+}
