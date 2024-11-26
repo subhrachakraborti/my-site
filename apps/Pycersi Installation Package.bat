6532a73fd63a870e027b6a7451924603bcc2fa89
Subhra Chakraborti <cers.subhra@gmail.com>
2024-11-26 14:49:03 +0530

    Add files via upload

---
+++ apps/Pycersi Installation Package.bat
@@ -0,0 +1,10 @@
+@echo on
+echo Checking Python and pip installation...
+python --version
+pip --version
+echo Installing pycersi...
+python -m pip install pycersi
+echo Latest version of PyCersi is successfully installed.
+echo Thank you for installing PyCersi.
+echo ©️ 2024-2025 PyCersi. All rights reserved.
+pause
