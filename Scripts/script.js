2252a2f39b627e58baa1a2dba3f8408a42c0e198
Subhra Chakraborti <cers.subhra@gmail.com>
2025-02-18 15:24:47 +0530

    Add files via upload

--- Scripts/script.js
+++ Scripts/script.js
@@ -61,10 +61,8 @@ document.addEventListener('DOMContentLoaded', function() {
         const minutes = String(now.getMinutes()).padStart(2, '0');
         const seconds = String(now.getSeconds()).padStart(2, '0');
         const timeString = `${hours}:${minutes}:${seconds}`;
-
         document.getElementById('time').textContent = timeString;
     }
-
     setInterval(updateTime, 1000);
     updateTime();
 });
